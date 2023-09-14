b33869a590e337c7217857d1ea71342031829abc
Marcelo Ramirez <ramirezyapura@gmail.com>
2023-09-14 09:59:32 -0400

    agregando un mensaje en la consola para registrar las solicitudes get

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const port = 3000;
 // Configurar Express para servir archivos estáticos desde la carpeta 'page'
 app.use(express.static(__dirname + '/page'));
 
+app.use((req, res, next) => {
+  const ipAddress = req.ip; // Obtiene la dirección IP del cliente
+
+  if (req.method === 'GET' && ipAddress === 'IP_ESPECIFICA') {
+    console.log(`Solicitud GET recibida desde la IP ${ipAddress}`);
+  }
+
+  next(); // Llama al siguiente middleware o enrutador
+});
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/page/index.html');
 });
