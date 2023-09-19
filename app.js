724111c0428311881b1dffeececf31756ec71169
Marcelo Ramirez <ramirezyapura@gmail.com>
2023-09-19 09:21:43 -0400

    test commit

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const express = require('express');
+const multer = require('multer');
 const app = express();
 const port = 3000;
 
 // Configurar Express para servir archivos estáticos desde la carpeta 'page'
 app.use(express.static(__dirname + '/page'));
 
-app.use((req, res, next) => {
-  const ipAddress = req.ip; // Obtiene la dirección IP del cliente
-
-  if (req.method === 'GET' && ipAddress === 'IP_ESPECIFICA') {
-    console.log(`Solicitud GET recibida desde la IP ${ipAddress}`);
-  }
-
-  next(); // Llama al siguiente middleware o enrutador
+// Configurar multer para manejar la carga de archivos
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Guarda los archivos en la carpeta 'uploads'
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname); // Nombre del archivo
+  },
 });
 
+const upload = multer({ storage });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/page/index.html');
+// Ruta para mostrar el formulario de carga de archivos
+app.get('/upload', (req, res) => {
+  res.sendFile(__dirname + '/page/upload.html');
+});
+
+// Ruta para manejar la carga de archivos desde el formulario
+app.post('/upload', upload.single('imagen'), (req, res) => {
+  // 'imagen' es el nombre del campo del formulario que contiene la imagen
+  res.send('Imagen subida con éxito.');
 });
 
 app.listen(port, () => {
